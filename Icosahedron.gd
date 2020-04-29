ed70b40cb59945130afa4801444f542288dee18c
Tomi Mäntylä <t0mm4n@gmail.com>
2020-04-29 17:09:31 +0300

    Ikosahedri tesseloitu!

--- Icosahedron.gd
+++ Icosahedron.gd
@@ -1,6 +1,7 @@
 extends MeshInstance
 
-export var edge_length=1.0
+#export var edge_length=1.0
+export var frequency = 1
 
 const phi = (1+sqrt(5))/2
 
@@ -8,6 +9,8 @@ var vertices = []
 var st = SurfaceTool.new()
 
 func _ready():
+	
+	var tri
 	
 	vertices.resize(12)
 	
@@ -29,38 +32,46 @@ func _ready():
 	#Top --Who says which vertex is on top?
 	st.add_color(Color.blue)
 	for i in range(1,6):
-		st.add_vertex(vertices[0])
-		st.add_vertex(vertices[i%5+1])
-		st.add_vertex(vertices[i])
+		#st.add_vertex(vertices[0])
+		#st.add_vertex(vertices[i%5+1])
+		#st.add_vertex(vertices[i])
+		tri = TriTess.new(st, vertices[0], vertices[i%5+1], vertices[i], frequency)
+		st = tri.get_st()
+	
 	#st.add_vertex(vertices[0])
 	#st.add_vertex(vertices[5])
 	#st.add_vertex(vertices[1])
 	
 		
 	#Bottom
-	for i in range(1, 6):
-		st.add_vertex(vertices[11])
-		st.add_vertex(vertices[i+5])
-		st.add_vertex(vertices[i%5+6])
+#	for i in range(1, 6):
+#		st.add_vertex(vertices[11])
+#		st.add_vertex(vertices[i+5])
+#		st.add_vertex(vertices[i%5+6])
+		tri = TriTess.new(st, vertices[11], vertices[i+5], vertices[i%5+6], frequency)
+		st = tri.get_st()
+#
+#	#Side
+#	st.add_color(Color.red)
+#	for i in range(1,6):
+#		st.add_vertex(vertices[i])
+#		st.add_vertex(vertices[i%5+1])
+#		st.add_vertex(vertices[i+5])
+		tri = TriTess.new(st, vertices[i], vertices[i%5+1], vertices[i+5], frequency)
+		st = tri.get_st()
+#
+#	st.add_color(Color.aquamarine)
+#	for i in range(1,6):
+#		st.add_vertex(vertices[i%5+1])
+#		st.add_vertex(vertices[i%5+6])
+#		st.add_vertex(vertices[i+5])
+		tri = TriTess.new(st, vertices[i%5+1], vertices[i%5+6], vertices[i+5], frequency)
+		st = tri.get_st()
+		
+	#st.generate_normals()
+	#mesh = st.commit()
 	
-	#Side
-	st.add_color(Color.red)
-	for i in range(1,6):
-		st.add_vertex(vertices[i])
-		st.add_vertex(vertices[i%5+1])
-		st.add_vertex(vertices[i+5])
-	
-	st.add_color(Color.aquamarine)
-	for i in range(1,6):
-		st.add_vertex(vertices[i%5+1])
-		st.add_vertex(vertices[i%5+6])
-		st.add_vertex(vertices[i+5])
-	
-	st.generate_normals()
-	mesh = st.commit()
-	
-	var tri = TriTess.new(Vector3(0,1,0), Vector3(1,0,0), Vector3(-1, 0, 0))
-	mesh=tri.get_mesh()
+	mesh=st.commit()
 	
 func _process(delta): 
 	rotate_y(delta)
@@ -69,7 +80,7 @@ class TriTess extends MeshInstance:
 
 	# Member variables
 	#export var side_length = 2.0
-	export(int) var freq = 3
+	var freq = 1
 	
 	var top 
 	var right
@@ -77,49 +88,54 @@ class TriTess extends MeshInstance:
 
 	var tri_mesh
 	var vertices = []
-	var st = SurfaceTool.new()
+	var st
 	
-	const hcoff = 0.86602540378
+	#const hcoff = 0.86602540378
 	
 	func meshify():
-		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		#st.begin(Mesh.PRIMITIVE_TRIANGLES)
 		#Ensimmäinen kolmio
 		st.add_color(Color.red)
+		st.add_normal(vertices[0][0])
 		st.add_vertex(vertices[0][0])
+		st.add_normal(vertices[1][1])
 		st.add_vertex(vertices[1][1])
+		st.add_normal(vertices[1][0])
 		st.add_vertex(vertices[1][0])
 		var f = freq+1
 		for i in range(1, freq):
 			for j in range(i+1):
 				#Ensin ylöspäin osoittavat kolmiot
 				st.add_color(Color.red)
+				st.add_normal(vertices[i][j])
 				st.add_vertex(vertices[i][j])
+				st.add_normal(vertices[i+1][j+1])
 				st.add_vertex(vertices[i+1][j+1])
+				st.add_normal(vertices[i+1][j])
 				st.add_vertex(vertices[i+1][j])
 				#Sitten alaspäin osoittavat kolmiot
 			for j in range(i):
 				st.add_color(Color.blue)
+				st.add_normal(vertices[i][j])
 				st.add_vertex(vertices[i][j])
+				st.add_normal(vertices[i][j+1])
 				st.add_vertex(vertices[i][j+1])
+				st.add_normal(vertices[i+1][j+1])
 				st.add_vertex(vertices[i+1][j+1])
-		st.generate_normals()
-		tri_mesh = st.commit()
+		#st.generate_normals()
 	
-	func get_mesh():
+	func get_st():
 		calculate_vertices()
-		return(tri_mesh)
+		return(st)
 	
-	func _init(v0, v1, v2):
-		
+	func _init(surface_tool, v0, v1, v2, frequence=1):
 		top = v0
-		right = v2
-		left = v1
+		right = v1
+		left = v2
+		freq = frequence
+		st = surface_tool
 		
 	func calculate_vertices():
-		
-		print("täällä ollaan!")
-		#var down = Vector3(-0.5*side_length, -1.0*hcoff*side_length, 0.0)
-		#var right = Vector3(side_length, 0.0, 0.0)
 		
 		var tl = left - top
 		var r = right - left
@@ -138,7 +154,7 @@ class TriTess extends MeshInstance:
 			#print(i)
 			for j in range(i+1):
 				#print("    "+str(j))
-				vertices[i][j]=(i*t_down + j*t_right)
+				vertices[i][j]=(top+i*t_down + j*t_right)
 		#print(vertices)
 		for i in range(f+1):
 			for j in range(i+1):
